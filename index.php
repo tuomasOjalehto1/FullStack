520a6b96ab6bda4f4a4e6f2cabdc167abcbfa38f
tuomasOjalehto1 <116883624+tuomasOjalehto1@users.noreply.github.com>
2024-04-28 21:17:27 +0300

    Indexi sivua korjailtu

--- index.php
+++ index.php
@@ -50,9 +50,9 @@ require_once 'Utils/connect.php';
   </div>
 
   <!-- Tähän se footer -->
-  <!-- <footer class="p-3 bg-dark text-white text-center">
+  <footer class="p-3 bg-dark text-white text-center">
     <p>&copy; 2024 Kiinteistöhuolto Oy</p>
-  </footer> -->
+  </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
