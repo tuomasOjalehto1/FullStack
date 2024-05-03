07b5ae1111ffb6050895576f2c2e4ffe7e852e99
ylhavuorisusanna <susanna.ylhavuori@gmail.com>
2024-05-03 11:10:22 +0300

    kirjautuskoodin yritelmää, ei toimi :)

--- kirjaudu_asukas.php
+++ kirjaudu_asukas.php
@@ -1,6 +1,50 @@
 <?php
+session_start();
+
+// Otetaan yhteys tietokantaan
 require_once 'Utils/connect.php';
-?>
+
+if(isset($_SESSION['kayttajatunnus'])){
+    header("location: index.php");
+  }
+  
+  if(isset($_POST['submit'])){
+    if($_POST['kayttajatunnus'] == '' OR $_POST['salasana'] == ''){
+      echo '<script>alert("Tietoja puuttuu!");</script>';
+    }else{
+      $kayttajatunnus = $_POST['kayttajatunnus'];
+      $salasana = $_POST['salasana'];
+  
+      // Haetaan käyttäjä tietokannasta käyttäjätunnuksen perusteella
+      $kirjaudu = $yhteys->prepare("SELECT * FROM kayttaja_ja_salasana WHERE kayttajatunnus = :kayttajatunnus");
+      $kirjaudu->execute([$kayttajatunnus]);
+  
+      $data = $kirjaudu->fetch(PDO::FETCH_ASSOC);
+  
+      // Tarkistetaan löytyykö käyttäjää
+      if($kirjaudu->rowCount() > 0){
+        // Tarkistetaan salasana
+        if(password_verify($salasana, $data['salasana'])){
+            // Tarkistetaan rooli
+            if($data['rooli'] == 1){
+                // Kirjautuminen onnistui ja rooli on 1, tallennetaan käyttäjäsessio ja ohjataan etusivulle
+                $_SESSION['kayttajatunnus'] = $data['kayttajatunnus'];
+                header("location: asukas.php");
+                echo "Kirjautunut";
+        }else{
+          echo '<script>alert("Käyttäjätunnus tai salasana on väärin!");</script>';
+        }
+      }else{
+        echo '<script>alert("Käyttäjätunnus tai salasana on väärin!");</script>';
+      }
+    }
+  }
+}
+  
+  ?>
+
+<?php require_once 'header.php'; ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,16 +63,16 @@ require_once 'Utils/connect.php';
     <div class="container mt-5">
         <div class="row justify-content-center">
             <main class="form-signin w-50 m-auto">
-            <form method="POST" action="XXXXX.php">
+            <form method="POST" action="kirjaudu_asukas.php">
                 <div class="mb-3 row">
                     <div class="form-floating">
-                        <input type="sposti" name="sposti" class="form-control" id="floatingInput" placeholder="nimi@esimerkki.fi">
-                        <label for="floatingInput">Sähköpostiosoite</label>
+                        <input type="text" name="kayttajatunnus" class="form-control" id="floatingInput" placeholder="nimi@esimerkki.fi">
+                        <label for="floatingInput">Käyttäjätunnus</label>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <div class="form-floating">
-                        <input type="salasana" name="salasana" class="form-control" id="floatingPassword" placeholder="Salasana">
+                        <input type="password" name="salasana" class="form-control" id="floatingPassword" placeholder="Salasana">
                         <label for="floatingPassword">Salasana</label>
                     </div>
                 </div>
