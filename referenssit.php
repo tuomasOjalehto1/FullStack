44286c0f632278c4b3809b9691bc4c1b55abbd88
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-03 20:56:44 +0300

    Create referenssit.php

---
+++ referenssit.php
@@ -0,0 +1,77 @@
+<?php
+require_once 'Utils/connect.php';
+?>
+<!--Tässä header-->
+<?php require_once 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+  <title>Referenssit</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Tutustu R. Autio Oy:n referensseihin: toimimme laajasti kiinteistöhuollon alueella. Katso menestyksekkäät projektimme ja asiakaskokemukset.">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container mt-4">
+        <h2 class="mb-3">Referenssimme</h2>
+        <div class="row">
+
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="img/aura1.jpg" class="card-img-top" alt="Lumenaurausta Helsingissä">
+                    <div class="card-body">
+                        <h5 class="card-title"><b> Talvikunnossapito - Helsinki</b></h5>
+                        <p class="card-text">
+                            <b> R. Autio Oy vastasi Helsingin kaupungin asuinalueen talvikunnossapidosta kaudella 2023-2024. </b> <br> <br> 
+                                Projektiin kuului lumenauraus ja hiekoitus, joiden ansiosta alueen kulkureitit pysyivät turvallisina ja kulkukelpoisina koko talven ajan. Käytössämme oli moderni kalusto, jotka on suunniteltu toimimaan tehokkaasti kaupunkiympäristössä. Nopea reagointimme säätilan muutoksiin varmisti asukkaiden ja liikenteen sujuvuuden.
+                            <br><br>
+                            <b> Asiakkaan palaute:</b>
+                                "Asuinalueemme talvikunnossapito on sujunut esimerkillisesti R. Autio Oy:n toimesta. Lumenauraus ja hiekoitus on toteutettu nopeasti säätilan niin vaatiessa, ja asukkaiden turvallisuus on pysynyt koko ajan etusijalla. Olemme erittäin tyytyväisiä saamaamme palveluun."
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="img/asunto1.jpg" class="card-img-top" alt="Loppusiivous">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>Loppusiivous - Toimisto, Helsinki</b></h5>
+                        <p class="card-text">
+                            <b>R. Autio Oy vastasi loppusiivouksesta Helsingin Toimistot Oy:n uudistiloissa. </b> <br> <br>
+                                Projekti käsitti kattavan siivouksen, sisältäen lattiat, ikkunat ja kalusteet. Siivous toteutettiin rakennusprojektin päätyttyä, valmistaen tilat välittömään käyttöönottoon. Käytimme korkealuokkaisia siivousaineita ja -välineitä. Huomioimme erityisesti toimiston modernit pinnat, kuten luonnonkiviset lattiat ja lasipinnat, varmistaen niiden käsittelyn oikeaoppisesti ja vahingoittamatta. Tiimimme suoritti työn huolellisesti ja aikataulussa, mikä mahdollisti asiakkaan muuton tiloihin ilman viivästyksiä.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="img/asunto3.jpg" class="card-img-top" alt="Kylpyhuone remontti">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>Kalusteiden asennus - Espoo</b></h5>
+                        <p class="card-text">
+                            <b>R. Autio Oy suoritti kattavan kylpyhuoneen kalusteiden asennuksen osana laajempaa remonttia Espoossa.</b><br><br>
+                                Varmistimme, että kaikki asennukset tehtiin huolellisesti ja ammattimaisesti, mikä takasi sekä esteettisen että toiminnallisen lopputuloksen. Erityistä huomiota kiinnitettiin vesieristykseen ja kalusteiden sijoitteluun, jotta saavutettaisiin sekä käytännöllinen että visuaalisesti miellyttävä kokonaisuus.
+                            <br><br>
+                            <b>Asiakkaan palaute</b>
+                            "Uudistettu kylpyhuoneemme on nyt sekä toiminnallinen että visuaalisesti houkutteleva. R. Autio Oy:n asentamat kalusteet ovat laadukkaita ja asennustyö oli ensiluokkaista. Erityisesti arvostimme ammattimaista otetta ja kykyä toteuttaa juuri toiveidemme mukainen lopputulos. Olemme erittäin tyytyväisiä ja suosittelemme heitä kaikille kylpyhuoneremonttia suunnitteleville."
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+  <!-- Footer -->
+  <?php require_once 'footer.php'; ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
