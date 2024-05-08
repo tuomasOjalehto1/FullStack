0928b91568f1370f1645d274e343822e7b1f9e31
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-08 22:47:50 +0300

    stylen muokkausta

--- kirjautumissivu.php
+++ kirjautumissivu.php
@@ -16,39 +16,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="p-5 bg-primary text-white text-center">
-        <h1>Kirjaudu</h1>
-        <h3>Asukas</h3>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <h2 class="mb-3 text-center">Kirjaudu</h2>
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <main class="form-signin w-50 m-auto">
+                        <form method="POST" action="Utils/kirjautuminen.php">
+                            <div class="mb-3 row">
+                                <div class="form-floating">
+                                    <input type="text" name="sposti" class="form-control" id="floatingInput" placeholder="nimi@esimerkki.fi">
+                                    <label for="floatingInput">Käyttäjätunnus</label>
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <div class="form-floating">
+                                    <input type="password" name="salasana" class="form-control" id="floatingPassword" placeholder="Salasana">
+                                    <label for="floatingPassword">Salasana</label>
+                                </div>
+                            </div>
+                            <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Kirjaudu</button>
+                            <!--<h6 class="mt-3">Jos sinulla ei ole käyttäjätunnusta  <a href="register.php">Luo tunnus</a></h6>-->
+                        </form>
+                    </main>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <main class="form-signin w-50 m-auto">
-            <form method="POST" action="Utils/kirjautuminen.php">
-                <div class="mb-3 row">
-                    <div class="form-floating">
-                        <input type="text" name="sposti" class="form-control" id="floatingInput" placeholder="nimi@esimerkki.fi">
-                        <label for="floatingInput">Käyttäjätunnus</label>
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <div class="form-floating">
-                        <input type="password" name="salasana" class="form-control" id="floatingPassword" placeholder="Salasana">
-                        <label for="floatingPassword">Salasana</label>
-                    </div>
-                </div>
-
-                <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Kirjaudu</button>
-                <!--<h6 class="mt-3">Jos sinulla ei ole käyttäjätunnusta  <a href="register.php">Luo tunnus</a></h6>-->
-            
-            
-            </form>
-            </main>
-          </div>
-      </div>
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
 
 <?php require_once 'footer.php'; ?>
-
