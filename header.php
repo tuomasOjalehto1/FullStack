ba96a2e7198a83a904af0642581fdfd3085b60f0
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-10 12:10:37 +0300

    lisätty muutos href

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="description" content="TEKSTIÄ">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="img/icon1.png" type="image/png">
-    <link href="headerstyle.css" rel="stylesheet">
+    <link href="Styles/headerstyle.css" rel="stylesheet">
 </head>
 <body>
     <header class="header text-center p-3 fixed-top">
--- isannoitsija.php
+++ isannoitsija.php
@@ -22,7 +22,7 @@ $stmt_tyontekijat = $yhteys->query($sql_tyontekijat);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="style.css" rel="stylesheet">
+    <link href="Styles/style.css" rel="stylesheet">
 </head>
 <body>
     <div class="p-5 bg-primary text-white text-center"> <!--CSS joku koti taustakuva -->
