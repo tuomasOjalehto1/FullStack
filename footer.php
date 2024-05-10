ee7165115348607dae4de0fca1d59f52c04d9a1c
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-10 15:58:16 +0300

    navbar toimii nyt

--- footer.php
+++ footer.php
@@ -1,49 +1,36 @@
-<!DOCTYPE html>
-<html lang="fi">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="Styles/footerstyle.css" rel="stylesheet"> 
-</head>
-<body>
-    <footer class="footer text-center p-3 fixed-bottom">
-        <div class="container">
-            <div class="row">
+<link href="Styles/footerstyle.css" rel="stylesheet"> 
 
-                <div class="col-md-6">
-                    <h5 class="mb-3">R. Autio Oy</h5>
-                    <p class="my-1">Y-tunnus: 0835677-3</p>
-                </div>
+<footer class="footer text-center p-3 fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h5 class="mb-3">R. Autio Oy</h5>
+                <p class="my-1">Y-tunnus: 0835677-3</p>
+            </div>
 
-                <!--<div class="col-md-4">
+             <!--<div class="col-md-4">
                     <h5 class="mb-3">Linkit</h5>
                     <ul class="list-unstyled text-center">
                         <li class="my-1"><a href="index.php" class="link-dark text-decoration-none">Etusivu</a></li> 
                         <li class="my-1"><a href="yhteystiedot.php" class="link-dark text-decoration-none">Yhteystiedot</a></li> 
                         <li class="my-1"><a href="otayhteytta.php" class="link-dark text-decoration-none">Ota yhteyttä</a></li> 
                         <li class="my-1"><a href="referenssit.php" class="link-dark text-decoration-none">Referenssit</a></li> 
-                        <li class="my-1"><a href="kirjaudu_asukas.php" class="link-dark text-decoration-none">Kirjaudu asukkaana</a></li>
-                        <li class="my-1"><a href="kirjaudu_isannoitsija.php" class="link-dark text-decoration-none">Kirjaudu isännöitsijänä</a></li>
-                        <li class="my-1"><a href="kirjaudu_tyontekija.php" class="link-dark text-decoration-none">Kirjaudu työntekijänä</a></li>
+                        <li class="my-1"><a href="kirjautumissivu.php" class="link-dark text-decoration-none">Kirjaudu sisään</a></li>
                     </ul>
                 </div>-->
 
-                <div class="col-md-6">
-                    <h5 class="mb-3">Yhteystiedot</h5>
-                    <p class="my-1">R. Autio Oy</p>
-                    <p class="my-1">Puhelinnumero: 010 1234 567</p>
-                    <p class="my-1">Sähköposti: info@kiinteistohuolto.fi</p>
-                    <p class="my-1">Pohjolankatu 23, 00610 Helsinki</p>
-                </div>
-
+            <div class="col-md-6">
+                <h5 class="mb-3">Yhteystiedot</h5>
+                <p class="my-1">R. Autio Oy</p>
+                <p class="my-1">Puhelinnumero: 010 1234 567</p>
+                <p class="my-1">Sähköposti: info@kiinteistohuolto.fi</p>
+                <p class="my-1">Pohjolankatu 23, 00610 Helsinki</p>
             </div>
         </div>
+    </div>
+    <div class="container text-center">
+        <p>&copy; <?php echo date("Y"); ?> R. Autio Oy. Kaikki oikeudet pidätetään.</p>
+    </div>
+</footer>
 
-        <div class="container text-center">
-            <p>&copy; <?php echo date("Y"); ?> R. Autio Oy. Kaikki oikeudet pidätetään.</p>
-        </div>
-    </footer>
-</body>
-</html>
+
