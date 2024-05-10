28ec6cc6d7b7d64fcca906afe24654f171c2f654
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-10 12:50:04 +0300

    siistitty

--- otayhteytta.php
+++ otayhteytta.php
@@ -1,37 +1,7 @@
-<?php
-require_once 'Utils/connect.php';
 
-// Tarkistetaan, onko lomake lähetetty
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Otetaan vastaan lomakkeen tiedot ja suodatetaan ne
-    $etunimi = filter_input(INPUT_POST, 'etunimi', FILTER_SANITIZE_STRING);
-    $sukunimi = filter_input(INPUT_POST, 'sukunimi', FILTER_SANITIZE_STRING);
-    $puhelin = filter_input(INPUT_POST, 'puhelin', FILTER_SANITIZE_STRING);
-    $yritys = filter_input(INPUT_POST, 'yritys', FILTER_SANITIZE_STRING);
-    $sposti = filter_input(INPUT_POST, 'sposti', FILTER_SANITIZE_EMAIL);
-    $viesti = filter_input(INPUT_POST, 'viesti', FILTER_SANITIZE_STRING);
-
-    // Valmistellaan SQL-lause tietojen tallentamista varten
-    try {
-        $stmt = $yhteys->prepare("INSERT INTO otayhteyttataulu (etunimi, sukunimi, puhelin, yritys, sposti, viesti) VALUES (:etunimi, :sukunimi, :puhelin, :yritys, :sposti, :viesti)");
-        $stmt->bindParam(':etunimi', $etunimi);
-        $stmt->bindParam(':sukunimi', $sukunimi);
-        $stmt->bindParam(':puhelin', $puhelin);
-        $stmt->bindParam(':yritys', $yritys);
-        $stmt->bindParam(':sposti', $sposti);
-        $stmt->bindParam(':viesti', $viesti);
-        
-        $stmt->execute();
-
-        $message = "<p class='alert alert-success text-center'>Viesti lähetetty onnistuneesti! Olemme sinuun pian yhteydessä.</p>";
-    } catch(PDOException $e) {
-        $message = "<p class='alert alert-danger text-center'>Tietokantaan lisääminen epäonnistui: " . $e->getMessage() . "</p>";
-    }
-}
-
-?>
 <!--Tässä header-->
 <?php require_once 'header.php'; ?>
+<?php require_once 'kasittele_yhdeydenotto.php'; ?>
 
 <!DOCTYPE html>
 <html lang="fi">
@@ -51,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="row justify-content-center">
                     <div class="col-sm-12 col-md-10 padding">
                         <p>Täytä alla oleva lomake, niin olemme sinuun yhteydessä. Keskustellaan lisää siitä, miten voimme olla avuksi.</p>
-                        <!-- onkohan tämä oikein -->
+
                         <form action="otayhteytta.php" method="post"> 
 
                             <div class="mb-3 mt-3">
