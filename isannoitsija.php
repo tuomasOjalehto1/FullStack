eae60f4af5ddb2aa26a6d386fcf19b704c3b2b0a
Susanna <s_naatanen@hotmail.com>
2024-05-11 20:35:19 +0300

    header_kirjautunut isännöitsijäsivuun

--- isannoitsija.php
+++ isannoitsija.php
@@ -10,7 +10,7 @@ $results = haeTehtavat($yhteys);
 ?>
 
 <!--Tässä header-->
-<?php require_once 'header.php'; ?>
+<?php require_once 'header_kirjautunut.php'; ?>
 
 <!DOCTYPE html>
 <html lang="fi">
