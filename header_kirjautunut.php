7161fcc9baadfef069cbec1ddafe30018fb386ee
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-13 22:33:17 +0300

    nyt toimii myös hampurilaisvalikon sulku

--- header_kirjautunut.php
+++ header_kirjautunut.php
@@ -1,13 +1,7 @@
 
-<!DOCTYPE html>
-<html lang="fi">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="TEKSTIÄ">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="img/icon1.png" type="image/png">
     <link href="Styles/headerstyle.css" rel="stylesheet">
+    <link rel="icon" href="img/icon1.png" type="image/png">
 </head>
 <body>
     <header class="header text-center p-3 fixed-top">
@@ -59,7 +53,3 @@
             </div>
         </div>
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
-
