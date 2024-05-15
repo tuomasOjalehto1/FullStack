5f089ebcc1d8045c3a2f13df1ef13efe167da4fd
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-15 13:38:40 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
             <div class="col-md-4">
                 <h5 class="mb-3">Yhteystiedot</h5>
                 <p class="my-1">Puhelin: 010 1234 567</p>
+                <p class="my-1">Päivystyspuhelin: 020 123 4567</p>
                 <p class="my-1">Sähköposti: info@kiinteistohuolto.fi</p>
                 <p class="my-1">Osoite: Pohjolankatu 23, 00610 Helsinki</p>
             </div>
