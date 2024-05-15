d1e273ad492a852643ebb9042b1ab369c28b845a
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-05-15 16:47:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -52,31 +52,28 @@ if (isset($_SESSION['sposti'])) {
             </div>
 
             <div class="col-md">
+                <br>
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-                    </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/piha.jpg" class="d-block w-100 img-fluid rounded" alt="Lehtien haravoiminen">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img/piha.jpg" class="d-block w-100 img-fluid rounded" alt="Lehtien haravoiminen">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/talonmies.jpg" class="d-block w-100 img-fluid rounded" alt="Talonmies työssään">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/siivous.jpg" class="d-block w-100 img-fluid rounded" alt="Siivoustyöt">
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="img/talonmies.jpg" class="d-block w-100 img-fluid rounded" alt="Talonmies työssään">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/siivous.jpg" class="d-block w-100 img-fluid rounded" alt="Siivoustyöt">
-                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </a>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </a>
             </div>
         </div>
     </div>
